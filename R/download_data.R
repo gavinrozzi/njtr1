bcb481d2b5894ca562b5f13fb198bf676d72f307
Gavin Rozzi <gr@gavinrozzi.com>
2021-01-16 22:51:58 -0500

    First working version w/ 3 tables implemented
    WIP

--- R/download_data.R
+++ R/download_data.R
@@ -2,92 +2,75 @@
 #'
 #'
 #' @param year Year to download crash data
+#' @param type The type of NJDOT crash data to download.
 #' @return tibble of all reported accidents for the year
 #' @examples
-#' get_crashes(2019)
+#' get_njtr1(year = 2019, type = 'Accidents')
 #' @importFrom readr read_csv
+#' @importFrom readr cols
+#' @importFrom stringr str_to_lower
+#' @importFrom lubridate mdy
 #' @export
-get_crashes <- function(year) {
-  fields <- read.csv('data/crash_fields_17_final.csv',header = FALSE)
-  base_url <- 'https://www.state.nj.us/transportation/refdata/accident/'
-  # Build download URL from function arguments
-  file_name <- paste0(base_url,year,'/NewJersey',year,'Accidents.zip')
-  # create a temporary directory and file
-  zip_file <- tempfile(fileext = ".zip")
-  td = tempdir()
-  download.file(file_name, zip_file, mode = "wb")
-  # Get name of first file in the ZIP archive
-  fname <- unzip(zip_file, list=TRUE)$Name[1]
-  # unzip the file to the temporary directory
-  unzip(zip_file, files=fname, exdir=td, overwrite=TRUE)
-  # Store path of extracted file
-  fpath = file.path(td, fname)
-  # Read in the file
-  data <- suppressWarnings(readr::read_csv(fpath, col_names = FALSE, trim_ws = TRUE,quote="", col_types = cols()))
-  # Add field names
-  names(data) <- fields$V1
-return(data)
-}
+get_njtr1 <- function(year, type) {
+  
+  # Convert Year to integer
+  year <- as.integer(year)
+  
+  # Set base URL for data downloads
+  base_url <- "https://www.state.nj.us/transportation/refdata/accident/"
+  
+  
+  if ( year >= 2017 ) {
+  # Set parameters using input
+  fields <- utils::read.csv(paste0("inst/extdata/fields/2017/",type,".csv"),header = FALSE)
+  file_name <- paste0(base_url,as.character(year),'/NewJersey',year,type,'.zip')
+  cat(file_name)
+  
+  } else if ( year <= 2016 ) {
+    
+    stop("Years prior to 2017 are not yet supported")
+    
+  } else if ( year > 2019 ) {
+    
+    stop("Years past 2019 are not yet supported")
+  }
 
-#' Download NJ car accident driver data for a given year
-#'
-#'
-#' @param year Year to download crash data
-#' @return tibble of all drivers involved in car accidents for the year
-#' @examples
-#' get_drivers(2019)
-#' @importFrom readr read_csv
-#' @export
-get_drivers <- function(year) {
-  fields <- read.csv('data/driver_fields_17_final.csv',header = FALSE)
-  base_url <- 'https://www.state.nj.us/transportation/refdata/accident/'
-  # Build download URL from function arguments
-  file_name <- paste0(base_url,year,'/NewJersey',year,'Drivers.zip')
-  # create a temporary directory and file
+  # create a temporary directory and file for downloading the data
   zip_file <- tempfile(fileext = ".zip")
-  td = tempdir()
-  download.file(file_name, zip_file, mode = "wb")
+  td <- tempdir()
+  utils::download.file(file_name, zip_file, mode = "wb")
+  
   # Get name of first file in the ZIP archive
-  fname <- unzip(zip_file, list=TRUE)$Name[1]
+  fname <- utils::unzip(zip_file, list=TRUE)$Name[1]
+  
   # unzip the file to the temporary directory
-  unzip(zip_file, files=fname, exdir=td, overwrite=TRUE)
+  utils::unzip(zip_file, files=fname, exdir=td, overwrite=TRUE)
+  
   # Store path of extracted file
-  fpath = file.path(td, fname)
+  fpath <- file.path(td, fname)
+
+  
   # Read in the file
-  data <- suppressWarnings(readr::read_csv(fpath, col_names = FALSE, trim_ws = TRUE,quote="", col_types = cols()))
+  data <- suppressWarnings(readr::read_csv(fpath, col_names = FALSE, trim_ws = TRUE,quote="", col_types = readr::cols()))
+  
   # Add field names
   names(data) <- fields$V1
+  
+  #data$crash_date <- lubridate::mdy(data$crash_date)
   return(data)
 }
 
-#' Download NJ car accident vehicle data for a given year
-#'
-#'
-#' @param year Year to download crash data
-#' @return tibble of all vehicles involved in accidents for the year
-#' @examples
-#' get_accidents(2019)
-#' @importFrom readr read_csv
-#' @export
-get_vehicles <- function(year) {
-  fields <- read.csv('data/vehicle_fields_17_final.csv',header = FALSE)
-  base_url <- 'https://www.state.nj.us/transportation/refdata/accident/'
-  # Build download URL from function arguments
-  file_name <- paste0(base_url,year,'/NewJersey',year,'Vehicles.zip')
-  # create a temporary directory and file
-  zip_file <- tempfile(fileext = ".zip")
-  td = tempdir()
-  download.file(file_name, zip_file, mode = "wb")
-  # Get name of first file in the ZIP archive
-  fname <- unzip(zip_file, list=TRUE)$Name[1]
-  # unzip the file to the temporary directory
-  unzip(zip_file, files=fname, exdir=td, overwrite=TRUE)
-  # Store path of extracted file
-  fpath = file.path(td, fname)
-  # Read in the file
-  data <- suppressWarnings(readr::read_csv(fpath, col_names = FALSE, trim_ws = TRUE,quote="", col_types = cols()))
-  # Add field names
-  names(data) <- fields$V1
-  return(data)
-}  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
