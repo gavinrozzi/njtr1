6eaa80a9587ca6f93981bedc5c02b5572910e4be
Gavin Rozzi <gr@gavinrozzi.com>
2021-04-09 12:23:20 -0400

    Fix data error, make internet downloads fail gracefully

--- R/download_data.R
+++ R/download_data.R
@@ -4,8 +4,7 @@
 #' @param year Year to download crash data
 #' @param type The table of NJDOT crash data to download
 #' @return tibble of all reported accidents for the year
-#' @importFrom readr read_csv
-#' @importFrom readr cols
+#' @importFrom httr http_error
 #' @importFrom stringr str_to_lower
 #' @importFrom lubridate mdy
 #' @examples
@@ -26,22 +25,28 @@ get_njtr1 <- function(year, type) {
   # Set base URL for data downloads
   base_url <- "https://www.state.nj.us/transportation/refdata/accident/"
 
-
+# Set column names based on year selected
   if (year >= 2017) {
     # Set parameters for download using input to function
     fields <- utils::read.csv(paste0(system.file("extdata", package = "njtr1"), "/fields/2017/", type, ".csv"), header = FALSE)
     file_name <- paste0(base_url, as.character(year), "/NewJersey", year, type, ".zip")
-  } else if (year <= 2016) {
-    stop("Years prior to 2017 are not yet supported")
-  } else if (year > 2019) {
-    stop("Years past 2019 are not yet supported")
-  }
+  } else if (year <= 2016 | year >= 2020) {
+    stop("Years prior to 2017 and past 2019 are not yet supported")
+  } 
 
   # create a temporary directory and file for downloading the data
   zip_file <- tempfile(fileext = ".zip")
   td <- tempdir()
-  utils::download.file(file_name, zip_file, mode = "wb")
-
+  
+  # Check if data is available and download the data
+  if (httr::http_error(file_name)) {
+    message("No internet connection or data source broken.")
+    return(NULL)
+  } else {
+    message("njtr1: downloading data")
+    utils::download.file(file_name, zip_file, mode = "wb")
+  }
+  
   # Get name of first file in the ZIP archive
   fname <- utils::unzip(zip_file, list = TRUE)$Name[1]
 
@@ -53,10 +58,19 @@ get_njtr1 <- function(year, type) {
 
 
   # Read in the file
-  data <- suppressWarnings(readr::read_csv(fpath, col_names = FALSE, trim_ws = TRUE, quote = "", col_types = readr::cols()))
+  data <- utils::read.csv(fpath, header=FALSE, row.names=NULL, quote="")
 
   # Add field names
   names(data) <- fields$V1
+  
+  # Parse dates
+  if (type == "Accidents") {
+    suppressWarnings(data$crash_date <- lubridate::mdy(data$crash_date))
+  } else if (type == "Pedestrians") {
+    suppressWarnings(data$date_of_birth <- lubridate::mdy(data$date_of_birth))
+  } else if (type == "Drivers") {
+    data$driver_dob <- suppressWarnings(lubridate::mdy(data$driver_dob))
+  }
 
   return(data)
 }
